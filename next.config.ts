5bd243134ec2642ef77083668e81baea37a2b364
kozo10110 <kd1363861@st.kobedenshi.ac.jp>
2026-06-26 13:16:50 +0900

    feat: APIのパスrewrites()設定#22

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8085/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
